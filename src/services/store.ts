2a316c6a2836b4f96b9f9289ee40ffb0eee7eeae
mac <azubike-junior>
2023-04-11 07:11:37 +0100

    changes store

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,10 +1,11 @@
-import { Action, AnyAction, configureStore, Dispatch, ThunkAction } from "@reduxjs/toolkit";
+import { Action, AnyAction, configureStore, Dispatch, ThunkAction, ThunkDispatch } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import loginUser from "./users/login";
 import modalsReducer from "./modals"
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import {Store} from 'redux'
 
-export const store = configureStore({
+export const store: Store = configureStore({
   reducer: {
     login: loginUser,
     modals: modalsReducer
@@ -15,10 +16,13 @@ setupListeners(store.dispatch);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
+
+export type AppThunkDispatch = ThunkDispatch<RootState, any, AnyAction>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppDispatch = () => useDispatch<AppThunkDispatch>();
+
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 
 export type AppThunk<ReturnType = void> = ThunkAction<
